3d2a110d1b32726739c02b105e445744a37e1128
oddgarden <oddgarden@outlook.com>
2023-02-16 04:43:26 +0000

    added new shell script - helloworld

---
+++ helloworld.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Script: Ops 201 Class 02 Ops Challenge Solution
+# Author: Lilian Mburu
+# Date of last revision: Feb 15th 2023
+# Purpose: Print a string to the terminal
+
+# Main
+
+    echo “I am working perfectly”
+    
+# End
