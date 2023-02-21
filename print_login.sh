5f0cdff1f4931ef1813dee1e44e64ca796561d48
oddgarden <oddgarden@outlook.com>
2023-02-20 22:20:41 -0500

    added script for class 02

---
+++ print_login.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Script: Ops 201 Class 03 Ops Challenge Solution
+# Author: Lilian Mburu
+# Date of last revision: Feb 20th 2023
+# Purpose: Prints the login history of users on this computer.
+
+# Main
+   all_logins() {
+        last
+    }
+
+    print_all_logins=all_logins
+
+    $print_all_logins
+
+# End
