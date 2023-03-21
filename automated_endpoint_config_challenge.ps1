533723a21dd7a17f0b560a740593fce93bfb5bd0
oddgarden <oddgarden@outlook.com>
2023-03-21 04:17:54 +0000

    adding scripts info

--- automated_endpoint_config_challenge.ps1
+++ automated_endpoint_config_challenge.ps1
@@ -7,4 +7,13 @@
 #>
 # Main
 
+# Enable File and Printer Sharing
+Set-NetFirewallRule -DisplayGroup "File And Printer Sharing" -Enabled True
+netsh advfirewall firewall add rule name="Allow incoming ping requests IPv4" dir=in action=allow protocol=icmpv4
+# Allow ICMP traffic
+# Enable Remote management
+# Remove bloatware
+# Enable Hyper-V
+# Disable SMBv1, an insecure protocol
+
 # End
