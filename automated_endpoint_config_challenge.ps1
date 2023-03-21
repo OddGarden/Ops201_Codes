c49a011414e418351a4531668bd290dc55200b3d
oddgarden <oddgarden@outlook.com>
2023-03-21 03:32:25 +0000

    adding challenge 11 - automating new windows 10 endpoint

---
+++ automated_endpoint_config_challenge.ps1
@@ -0,0 +1,10 @@
+<# 
+    Script:                   Ops 201 Class 10 Ops Challenge Solution
+    Author:                   Lilian Mburu
+    Date of last revision:    Mar 20th 2023
+    Purpose:                 A script that automates the configuration of a new Windows 10 endpoint
+
+#>
+# Main
+
+# End
